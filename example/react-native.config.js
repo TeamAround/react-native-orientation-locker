9ffa888ba0a4813c9a92de8cf47d60960cc8d2b5
Tomasz Baranowicz <tbaranowicz@gmail.com>
2022-04-25 12:51:57 +0200

    Get rid of Windows dependencies

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -17,18 +17,10 @@
 
 const path = require('path');
 
-const windowsSwitch = '--use-react-native-windows';
-if (process.argv.includes(windowsSwitch)) {
-  process.argv = process.argv.filter(arg => arg !== windowsSwitch);
-  process.argv.push('--config=metro.config.windows.js');
-  module.exports = {
-    reactNativePath: 'node_modules/react-native-windows',
-  };
-} else {
-  module.exports = {
-    project: {
-      android: {sourceDir: './android'},
-      ios: {project: './ios/example.xcodeproj'},
-    },
-  };
-}
+module.exports = {
+  project: {
+    android: {sourceDir: './android'},
+    ios: {project: './ios/example.xcodeproj'},
+  },
+};
+
